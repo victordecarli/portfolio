b4157d4feb2bd5b5ec1d4198d78ae388185782f7
V0nor <vonorvictor@gmail.com>
2023-12-19 11:42:18 -0200

    Removi arquivos não utilizados

--- src/components/sections/projects/projectData.jsx
+++ src/components/sections/projects/projectData.jsx
@@ -12,15 +12,5 @@ const Data = [
     cta: <CtaSvg />,
     progresso: 100,
   },
-  {
-    id: 2,
-    subTitle: 'Projeto em destaque',
-    title: 'Calculadora de orçamento',
-    desc: 'Um aplicativo web que permite fazer orçamentos. Veja o valor total do orçamento com desconto, lucro, simulação de taxas de pagamentos débito e crédito. Cadastre seus clientes e muito mais.',
-    techs: ['React', 'Tailwind', 'Vite'],
-    github: <GithubSvg />,
-    cta: <CtaSvg />,
-    progresso: 50,
-  },
 ];
 export default Data;
