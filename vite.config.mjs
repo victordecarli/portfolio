133011d9e6312fe31f3f23f2aa6440fa88986a26
victordecarli <vonorvictor@gmail.com>
2025-03-03 11:09:23 -0400

    [DEPS] Adiciona Typescript e shadcn/ui

---
+++ vite.config.mjs
@@ -0,0 +1,26 @@
+import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      external: [],
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  esbuild: {
+    jsxInject: `import React from 'react'`,
+  },
+  server: {
+    host: '0.0.0.0',
+  },
+});
