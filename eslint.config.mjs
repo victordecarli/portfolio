4d3018a90770f23c1da2ec6b644074e407380d9a
victordecarli <vonorvictor@gmail.com>
2026-01-07 20:32:44 -0400

    Chore: Remove dependencias nao utilizadas, atualiza as instaladas e reajusta a configuracao do eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,8 @@
-import eslint from '@eslint/js'
-import stylisticTs from '@stylistic/eslint-plugin-ts'
-import tseslint from 'typescript-eslint'
+import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
+export default defineConfig(
   eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  ...tseslint.configs.stylisticTypeChecked,
-  {
-    plugins: {
-      '@stylistic/ts': stylisticTs,
-    },
-  },
-  {
-    rules: {
-      '@typescript-eslint/no-unused-vars': [
-        'error',
-        {
-          args: 'all',
-          argsIgnorePattern: '^_',
-          caughtErrors: 'all',
-          caughtErrorsIgnorePattern: '^_',
-          destructuredArrayIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
-          ignoreRestSiblings: true,
-        },
-      ],
-    },
-  },
-)
+  tseslint.configs.recommended,
+);
